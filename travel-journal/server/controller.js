004430e86b84ca6635e960c7caf838e9a069d808
Emily Simpson <emilousim@gmail.com>
2021-11-14 22:28:27 -0500

    assessment 5 2nd update

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -26,7 +26,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+           create table cities(
+               city_id: serial, primary key;
+               name: varchar;
+               rating: integer;
+               country_id: integer foreign key references countries(country_id)
+           )
 
             insert into countries (name)
             values ('Afghanistan'),
